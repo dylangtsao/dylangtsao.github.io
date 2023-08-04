2b853544b43dc44e22c8f5b93a4874470f9ebc5f
Dylan Tsao <92745437+dylangtsao@users.noreply.github.com>
2023-08-04 16:22:33 -0700

    added new projects and improved layout

--- projects/projects.js
+++ projects/projects.js
@@ -4,20 +4,16 @@ const titles = document.querySelectorAll('.title');
 const images = document.querySelectorAll('.image');
 const descriptions = document.querySelectorAll('.description');
 const buttons = document.querySelectorAll('.link');
-
+const backdrop = document.querySelector('.backdrop');
 for (let i = 0; i < containers.length; i++) {
     containers[i].addEventListener('mousemove', (e) => {
         let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
         let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
-        // console.log(xAxis);
-        if (i==1) {
-            project[i].style.transform = `rotateY(${xAxis}deg) rotateX(${(yAxis/3)}deg)`;
-        } else if (i==2) {
-            project[i].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis/6.5}deg)`;
-        } else {
+        if (i==0) {
             project[i].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+        } else {
+            project[i].style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis/(3*i)}deg)`;
         }
-        
     });
     //mouse enter
     containers[i].addEventListener('mouseenter', (e) => {
@@ -35,6 +31,14 @@ for (let i = 0; i < containers.length; i++) {
         images[i].style.transform = `translateZ(0px)`;
         descriptions[i].style.transform = `translateZ(0px)`;
         buttons[i].style.transform = `translateZ(0px)`;
+    });
+    images[i].addEventListener('click', (e) => {
+        backdrop.style.display='block';
+        images[i].style.maxWidth = "100vw";
+    });
+    backdrop.addEventListener('click', (e) => {
+        backdrop.style.display='none';
+        images[i].style.maxWidth = "75vw";
     });
 }
 
