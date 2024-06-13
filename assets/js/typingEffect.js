a38740d0439c52ba9133ad1afa13f9736bd99c7e
Dylan Tsao <92745437+dylangtsao@users.noreply.github.com>
2024-06-12 19:28:09 -0700

    updated intro section phrases

--- assets/js/typingEffect.js
+++ assets/js/typingEffect.js
@@ -1,34 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  const terms = [
+    "UIUC Senior",
+    "Software Engineer",
+    "Developer",
+    "Learner",
+    "Math&CS Student",
+  ];
 
-    const terms = ["Student", "Software Engineer", "Developer"];
-    
-    let termIndex = 0;
-    let charIndex = 0;
-    const typingSpeed = 150;
-    const erasingSpeed = 120;
-    const delayBetweenTerms = 2000;
-    const typingElement = document.querySelector('.typing-effect');
+  let termIndex = 0;
+  let charIndex = 0;
+  const typingSpeed = 150;
+  const erasingSpeed = 120;
+  const delayBetweenTerms = 2000;
+  const typingElement = document.querySelector(".typing-effect");
 
-    function type() {
-        if (charIndex < terms[termIndex].length) {
-            typingElement.textContent += terms[termIndex].charAt(charIndex);
-            charIndex++;
-            setTimeout(type, typingSpeed);
-        } else {
-            setTimeout(erase, delayBetweenTerms);
-        }
+  function type() {
+    if (charIndex < terms[termIndex].length) {
+      typingElement.textContent += terms[termIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingSpeed);
+    } else {
+      setTimeout(erase, delayBetweenTerms);
     }
+  }
 
-    function erase() {
-        if (charIndex > 0) {
-            typingElement.textContent = terms[termIndex].substring(0, charIndex - 1);
-            charIndex--;
-            setTimeout(erase, erasingSpeed);
-        } else {
-            termIndex = (termIndex + 1) % terms.length;
-            setTimeout(type, typingSpeed);
-        }
+  function erase() {
+    if (charIndex > 0) {
+      typingElement.textContent = terms[termIndex].substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, erasingSpeed);
+    } else {
+      termIndex = (termIndex + 1) % terms.length;
+      setTimeout(type, typingSpeed);
     }
+  }
 
-    setTimeout(type, delayBetweenTerms);
+  setTimeout(type, delayBetweenTerms);
 });
